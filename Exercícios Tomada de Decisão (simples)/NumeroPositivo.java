6405a50b751479077be6209580dfd10e451e4994
Memyllyss <efktop@gmail.com>
2025-10-16 17:07:55 -0300

    Add files via upload

---
+++ Exercícios Tomada de Decisão (simples)/NumeroPositivo.java
@@ -0,0 +1,22 @@
+package github;
+
+import java.util.Scanner;
+
+public class NumeroPositivo {
+    public static void main(String[] args) {
+        Scanner entrada = new Scanner(System.in);
+
+        System.out.print("Digite um número inteiro: ");
+        int numero = entrada.nextInt();
+
+        if (numero > 0) {
+            System.out.println("O número é positivo.");
+        } else if (numero < 0) {
+            System.out.println("O número é negativo.");
+        } else {
+            System.out.println("O número é neutro (zero).");
+        }
+
+        entrada.close();
+    }
+}
